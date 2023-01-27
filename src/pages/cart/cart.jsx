879b3525190d6b4c9ac1f6332c75945609dbf262
eforrester <esfoures@gmail.com>
2023-01-27 12:12:14 -0500

    Empty cart display

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -2,10 +2,14 @@ import React, { useContext } from 'react';
 import { PRODUCTS } from '../../products';
 import  { CartItem }  from './cartItem';
 import { ShopContext } from '../../context/shop-context';
+import { useNavigate } from "react-router-dom"; 
 
 
 export const Cart = () => {
-  const { cartItems } = useContext(ShopContext); 
+  const { cartItems, getTotalCartAmount } = useContext(ShopContext); 
+  const totalAmount = getTotalCartAmount();
+  const navigate = useNavigate();
+
   return (
     <div className="cart">
       <div>
@@ -17,6 +21,15 @@ export const Cart = () => {
             return <CartItem data={product} />; 
           }
         })}
+        {totalAmount > 0 ? (
+          <div className="checkout">
+            <p>Subtotal: ${totalAmount}</p>
+            <button onClick={() => navigate("/")}>Continue Shopping</button>
+            <button>Checkout</button>
+          </div>
+        ) : (
+          <h1>Your Cart is Empty</h1>
+        )}
       </div>
     </div>
   )

a4af32dbd5ea8f5c1aaf6fd6c60e83d05c8f2d3c
eforrester <esfoures@gmail.com>
2023-02-01 12:04:28 -0500

    Horizontal scrolling added to product container

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -10,13 +10,15 @@ const Shop = () => {
     <div className="shop">
       {
         category.map((category, i) => 
-          <div key={i} className={`product-section ${category}`}>
-          <div className="title-container">
-            <h2>{category.toUpperCase()}</h2>
-            <img alt="arrow" src={require("../../assets/arrow.png")}></img>
-          </div>
-          <div className="product-container">
-          {PRODUCTS.filter(p => p.type === category).map(p => <Product data={p} />)}
+        <div className="category-section">
+            <div key={i} className={`product-section ${category}`}>
+              <div className="category-title-container">
+                <h2>{category.toUpperCase()}</h2>
+                <img alt="arrow" src={require("../../assets/arrow.png")}></img>
+              </div>
+              <div className="products-container">
+                {PRODUCTS.filter(p => p.type === category).map(p => <Product data={p} />)}
+              </div>
           </div>
         </div> 
         )

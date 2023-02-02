e0768a02b11a3833a6d013f7357d9727c09bc1e0
eforrester <esfoures@gmail.com>
2023-02-02 15:18:57 -0500

    Fix images in cart

--- src/pages/cart/cartItem.jsx
+++ src/pages/cart/cartItem.jsx
@@ -2,13 +2,13 @@ import React, { useContext } from "react";
 import { ShopContext } from "../../context/shop-context";
 
 export const CartItem = (props) => {
-  const { id, productName, price, productImage } = props.data;
+  const { id, productName, price, productImage, type } = props.data;
   const { cartItems, addToCart, removeFromCart, updateCartItemCount } =
     useContext(ShopContext);
 
   return (
     <div className="cartItem">
-      <img src={productImage} />
+      <img className="product-img" src={require(`../../assets/${type}/${productImage}`)} alt={productName}/>
       <div className="description">
         <p>
           <b>{productName}</b>

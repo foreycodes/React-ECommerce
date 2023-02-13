5e9aebbcfee794381d3909fe05615856d3b08344
eforrester <esfoures@gmail.com>
2023-02-12 21:41:43 -0500

    File setup

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -6,7 +6,6 @@ import "./button.css"
 const Button = ({ variant, text = "Shop collection" }) => {
   return (
     <motion.button 
-    whileHover={{ scale: 1.02 }}
     whileTap={{scale: 0.98}}
     className={`button ${ variant }`}>
         { text }
